3c700bb4390323a85d1dcebaa4a0e9551acd980d
Jonathon Vandezande <jevandezande@gmail.com>
2021-09-22 10:39:03 -0400

    Adds license options.

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,20 +1,40 @@
+from __future__ import annotations
+
 import sys
 from subprocess import check_call
+
+
+licenses = {
+    "apache-2",
+    "bsd-3-clause",
+    "lgpl-3.0",
+    "mit",
+    None,
+}
 
 
 def set_python_version():
     python_version = f"{sys.version_info.major}.{sys.version_info.minor}"
 
     file_names = [
-        "Dockerfile",
         "Pipfile",
         ".github/workflows/test.yml",
     ]
+
     for file_name in file_names:
         with open(file_name) as f:
             contents = f.read().replace(r"{python_version}", python_version)
         with open(file_name, "w") as f:
             f.write(contents)
+
+
+def set_license(license: Optional[str] ="MIT"):
+    if license is None:
+        return
+    license = license.lower()
+    if license not in licenses:
+        raise ValueError(f"{license=} is not available yet")
+    shutil.copy(f"licenses/{license}", "{{cookiecutter.repo_name}}/LICENSE")
 
 
 def git_init():
@@ -43,6 +63,7 @@ TERMINATOR = "\x1b[0m"
 
 def main():
     set_python_version()
+    set_license()
     git_init()
     install_dev()
     git_hooks()
